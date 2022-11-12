9db967c63a591c69ee2ee744523314b7058a33a9
lhqlovelsz <lhqlsz@126.com>
2022-11-12 19:41:47 +0800

    del

--- linuxfile/install_arch.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cp pacman.conf /etc/
-cp mirrorlist /etc/pacman.d/
-cp config /home/lhq/.config/i3/
-pacman -Sy
-pacman -S man-db man-pages texinfo vim sudo networkmanager dhcpcd nm-connection-editor network-manager-applet fcitx5 fcitx5-chinese-addons fcitx5-chewing fcitx5-im xorg i3-wm sddm i3status i3lock ttf-dejavu wqy-zenhei grub efibootmgr feh calibre 
-pacman -S archlinuxcn-keying
-pacman -S  yay
